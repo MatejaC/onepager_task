3bb701afc7de6d4d64a4f2e6a016c113cf91ee82
Mateja Cimpersak <matejacimpersak@gmail.com>
2021-07-20 18:59:00 +0200

    trying to apply JS intersection observer method regarding adding second navigation when scrolling

---
+++ script.js
@@ -0,0 +1,24 @@
+const header = document.querySelector(".header1");
+const sectionOne = document.querySelector("main");
+
+const sectionOneOptions = {
+  threshold: 0,
+  root: null,
+  rootMargin: "-450px 0px 0px 0px",
+};
+
+const sectionOneObserver = new IntersectionObserver(function (
+  entries,
+  sectionOneObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      header.classList.add("header-scrolled");
+    } else {
+      header.classList.remove("header-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
